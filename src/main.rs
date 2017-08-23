6c8dd5fd2704a8190b33655a3b4146c587b3820e
Emile Yvan Boutet Livoff <blemille@hotmail.com>
2017-08-23 14:25:39 +0200

    add example discord stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate discord;
+
+use discord::Discord;
+use discord::model::Event;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let discord = Discord::from_bot_token(
+        &env::var("DISCORD_TOKEN").expect("Expected token"),
+    ).expect("login failed");
+
+    let (mut connection, _) = discord.connect().expect("connect failed");
+
+    println!("Ready.");
+
+    loop {
+        match connection.recv_event() {
+            Ok(Event::MessageCreate(message)) => {
+                println!("{} says: {}", message.author.name, message.content);
+                if message.author.name == "FuzzyLitchi" {
+                    let _ = discord.send_message(message.channel_id, message.content.as_str(), "", false);
+                }
+            }
+            Ok(_) => {}
+            Err(discord::Error::Closed(code, body)) => {
+                println!("Gateway closed on us with code {:?}: {}", code, body);
+                break
+            }
+            Err(err) => println!("Receive error: {:?}", err)
+        }
+    }
 }
